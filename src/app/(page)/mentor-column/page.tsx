2a022da7bcdd9665d36745295f87439ea25f0d45
jbeat <jbeat0630@gmail.com>
2025-01-14 16:23:17 +0900

    chore: 목록페이지에서 에러발생시 not-found 매핑

--- src/app/(page)/mentor-column/page.tsx
+++ src/app/(page)/mentor-column/page.tsx
@@ -20,13 +20,16 @@ async function getMentorColumn() {
     const response = await fetch(process.env.API_URL + '/mentor-column');
 
     if (!response.ok) {
-      throw new Error('서버 에러 발생');
+      return null; // 응답이 실패한 경우 null 반환
     }
 
-    return response.json();
+    const data = await response.json();
+
+    // 데이터가 없으면 null 반환
+    return data || null;
   } catch (e) {
     console.error('API 호출 중 에러 발생:', e);
-    return [];
+    return null; // 에러 발생 시 null 반환
   }
 }
 
@@ -38,23 +41,26 @@ export default async function Page() {
         <div className="container">
           <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {data.map((post: PostData) => (
-              <div key={post.id} className="mb-10 overflow-hidden rounded-lg bg-white shadow-1 duration-300 hover:shadow-3">
-                <div className="relative w-full h-[220px]">
-                  <Image
-                      src={post.mainImage}
-                      alt={post.title}
-                      fill
-                      quality={75}
-                      style={{objectFit: 'cover'}}
-                  />
-                </div>
+                <div key={post.id}
+                     className="mb-10 overflow-hidden rounded-lg bg-white shadow-1 duration-300 hover:shadow-3">
+                  <div className="relative w-full h-[220px]">
+                    <Image
+                        src={post.mainImage}
+                        alt={post.title}
+                        fill
+                        quality={75}
+                        style={{objectFit: 'cover'}}
+                    />
+                  </div>
 
-                <div className="p-6 text-center sm:p-7 md:p-5 xl:p-7">
-                  <Link className="mb-4 text-gray-700 block text-xl font-semibold hover:text-primary sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]" href={`mentor-column/${post.id}`}>{post.title}</Link>
-                  <p className="text-gray-700 mb-4 text-base leading-relaxed">{post.type} {post.creData}</p>
-                </div>
+                  <div className="p-6 text-center sm:p-7 md:p-5 xl:p-7">
+                    <Link
+                        className="mb-4 text-gray-700 block text-xl font-semibold hover:text-primary sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                        href={`/mentor-column/${post.id}`}>{post.title}</Link>
+                    <p className="text-gray-700 mb-4 text-base leading-relaxed">{post.type} {post.creData}</p>
+                  </div>
 
-              </div>
+                </div>
             ))}
           </div>
         </div>
