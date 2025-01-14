6eae5f1785e6ceb7ad9c53bb9b29efe0e1b0e68f
jbeat <jbeat0630@gmail.com>
2025-01-14 09:59:14 +0900

    feature: 목업데이터 추가

---
+++ src/lib/post.js
@@ -0,0 +1,29 @@
+export const posts = [
+  {
+    id: 1,
+    name: 'Ansh Kyal',
+    title: 'Journey from ICSE to JEE: Overcoming Challenges',
+    user_type: 'MENTOR',
+    content: 'This is my first post!',
+    thumbnail: 'https://static.wematchnow.com/common/mentor_column/mock/column/Ansh_Kyal/user_thumbnail.jpg',
+    mainImage: 'https://static.wematchnow.com/common/mentor_column/dev/thumbnail_image_list/Ansh_Kyal_pc.png',
+  },
+  {
+    id: 2,
+    name: 'Rajendra Kumar',
+    title: 'Defying the Odds: My JEE Journey',
+    user_type: 'MENTOR',
+    content: 'This is my second post!',
+    thumbnail: 'https://static.wematchnow.com/common/mentor_column/mock/column/Rajendra_Kumar/user_thumbnail.jpg',
+    mainImage: 'https://static.wematchnow.com/common/mentor_column/dev/thumbnail_image_list/Rajendra_Kumar_pc.png',
+  },
+  {
+    id: 3,
+    name: 'Ansh Kyal',
+    title: 'Journey from ICSE to JEE: Overcoming Challenges',
+    user_type: 'MENTOR',
+    content: 'This is my third post!',
+    thumbnail: 'https://static.wematchnow.com/common/mentor_column/mock/column/Ansh_Kyal/user_thumbnail.jpg',
+    mainImage: 'https://static.wematchnow.com/common/mentor_column/dev/thumbnail_image_list/Ansh_Kyal_pc.png',
+  },
+];
